0c5eb782d5871da1711330207133ef69ee501e1e
Cesar Morales <cesarlenin2@live.com>
2020-06-17 10:21:27 -0700

    set up for number 5

--- drills.js
+++ drills.js
@@ -107,21 +107,9 @@ function main(){
 
   //5. Implement different tree traversals
   const BST = new BinarySearchTree();
-  BST.insert(25,25);
-  BST.insert(15);
-  BST.insert(50);
-  BST.insert(10);
-  BST.insert(24);
-  BST.insert(35);
-  BST.insert(70);
-  BST.insert(4);
-  BST.insert(12);
-  BST.insert(18);
-  BST.insert(31);
-  BST.insert(44);
-  BST.insert(66);
-  BST.insert(90);
-  BST.insert(22);
+  const dataset = [25, 15, 50, 10, 24, 35, 70, 4, 12, 18, 31, 44, 66, 90, 22];
+  dataset.forEach(data => BST.insert(data, data));
+  
   console.log(BST.inOrder());
   // console.log(BST.preOrder());
   // console.log(BST.postOrder());
