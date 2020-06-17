6921520adcb2bccdfae415e9762b4d88e743aeb2
Cesar Morales <cesarlenin2@live.com>
2020-06-17 09:54:35 -0700

    set up for number 5

--- drills.js
+++ drills.js
@@ -1,3 +1,4 @@
+const BinarySearchTree = require('./BinarySearchTree');
 
 function linearCounter (arr,value){
   let count=1;
@@ -83,5 +84,24 @@ function main(){
   //   5    6   9   11   
 
   //4.b= 5,6,7,10,9,11,8
+
+  //5. Implement different tree traversals
+  const BST = new BinarySearchTree();
+  BST.insert(25);
+  BST.insert(15);
+  BST.insert(50);
+  BST.insert(10);
+  BST.insert(24);
+  BST.insert(35);
+  BST.insert(70);
+  BST.insert(4);
+  BST.insert(12);
+  BST.insert(18);
+  BST.insert(31);
+  BST.insert(44);
+  BST.insert(66);
+  BST.insert(90);
+  BST.insert(22);
+  
 }
 const mainRun = main();
