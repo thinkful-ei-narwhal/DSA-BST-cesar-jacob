e8ef76c1ac5d604fb82622c4bb2cf46d40307963
Jacob Flaxman <jacobflaxman1@gmail.com>
2020-06-17 09:24:04 -0700

    num 3

--- drills.js
+++ drills.js
@@ -27,16 +27,42 @@ function binarySearch(array, value, start=0, end=array.length-1,count=1) {
   }
 }
 
+
+
+//Question 3 ____________________________________________________________________________
+
+//find a book in the dewey decimal system, provided with dewey number and title
+//dewey is a 3 digit number containing info about the book 0 - 999
+//look up book by dewey, find dewey, arr[dewey] = book
+const arr = [{dewey: 001, title: 'A book'}, {dewey: 002, title: 'A book1'}, {dewey: 003, title: 'A book2'}]
+
+function findBook(dewey, start = 0, end = arr.length) {
+
+  let index = Math.floor((start + end) / 2);
+
+  if (start > end) return -1;
+
+  if(arr[index].dewey === dewey) {
+    return arr[index].title
+  }
+  else if(arr[index].dewey < dewey) {
+    return findBook(dewey, title, index + 1, end)
+  }
+  else if (arr[index].dewey > dewey) {
+    return findBook(dewey, title, start, index - 1)
+  }
+}
+
 function main(){
   //1. How many searches?
   //A. 11,6,8
   // b.11,14,15
 
   //2. Adding a React UI
-  let arr= [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
-  const sortedArr = arr.sort((a,b) => a - b);
-  console.log('2.a: '+ linearCounter(arr, 13));
-  console.log('2.b: '+ binarySearch(sortedArr, 13));
-  
+  // let arr= [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
+  // const sortedArr = arr.sort((a,b) => a - b);
+  // console.log('2.a: '+ linearCounter(arr, 13));
+  // console.log('2.b: '+ binarySearch(sortedArr, 13));
+  console.log(findBook(002, 'A book1'))
 }
 const mainRun = main();
